5d2b8dbda1d3cf0bde86da2ed84fcae7e2e8489c
Manuel Jesús Rodriguez Arabi <manupb@gmail.com>
2020-10-04 23:26:03 +0200

    Intent y paso de parametros

--- app/src/main/java/com/example/intentexplicito/MainActivity.java
+++ app/src/main/java/com/example/intentexplicito/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.intentexplicito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void iniciarSecondaryActivity(View view) {
+        /*Creamos el objeto Intent*/
+        Intent intentSecondaryActivity = new Intent(this, SecondaryActivity.class);
+
+        //Paso de parámetros
+        intentSecondaryActivity.putExtra("nombre", "Manuel Rodríguez");
+        intentSecondaryActivity.putExtra("edad", 40);
+        intentSecondaryActivity.putExtra("telefono", "+34666112233");
+
+        //Llamamos al método StartActivity, que recibe el objeto Intent creado
+        startActivity(intentSecondaryActivity);
     }
 }
--- app/src/main/java/com/example/intentexplicito/SecondaryActivity.java
+++ app/src/main/java/com/example/intentexplicito/SecondaryActivity.java
@@ -3,6 +3,7 @@ package com.example.intentexplicito;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondaryActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SecondaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+
+        //Recogida de parámetros que recibimos del activity principal
+        Bundle datos = getIntent().getExtras();
+
+        Toast.makeText(this, "Nombre: " + datos.getString("nombre") + " Edad: "
+                + datos.getInt("edad") + " Teléfono: "
+                +datos.getString("telefono"), Toast.LENGTH_LONG).show();
     }
 }
